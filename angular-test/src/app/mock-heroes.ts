997365ffa9e512f27ec0162a49c3a86534a1582e
Arthur-geek <kofrathur@gmail.com>
2022-08-08 14:34:25 +0000

    first-chapter

---
+++ angular-test/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES : Hero[] = [
+    { id: 10, name: 'DR. Martin'},
+    { id: 11, name: 'Super Richard'},
+    { id: 12, name: 'Superman'},
+    { id: 13, name: 'Batman'},
+    { id: 14, name: 'Joker'},
+    { id: 15, name: 'Hulk'},
+    { id: 16, name: 'Thor'},
+    { id: 17, name: 'Iron man'},
+    { id: 18, name: 'Spiderman'},
+    { id: 19, name: 'Arthur'},
+];
